d5991b4d24358c0b2a6144623d27b9480632ce5a
Sascha Markus <sascha.markus@gmail.com>
2019-01-07 21:57:20 +0100

    paths

--- publishconf.py
+++ publishconf.py
@@ -14,27 +14,17 @@ from pelicanconf import *
 #SITEURL = 'https://www.der-flaneur.rocks'
 #SITEURL = ''
 SITEURL = 'https://saschamarkus.gitlab.io/flaneur'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
-THEME_STATIC_DIR = path.join(SITEURL, 'static')
-
-M_SITE_LOGO = path.join(SITEURL, "/images/flaneur.png")
 
 M_LINKS_FOOTER1 = [('Der Flaneur', '/'),
                 ('Über den Flaneur', path.join(SITEURL, 'pages/der-flaneur.html')),
                 ('Impressum', path.join(SITEURL, 'pages/impressum.html')),
                 ('Datenschutz', path.join(SITEURL, 'pages/datenschutz.html'))]
 
-M_CSS_FILES = ['https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700,700i%7CSource+Code+Pro:400,400i,600',
-                               path.join(SITEURL, '/static/m-light.css')]
-
-STATIC_PATHS = [
-                path.join(SITEURL, 'images'),
-                path.join(SITEURL, 'extra'),
-                ]
 
 DELETE_OUTPUT_DIRECTORY = True
 
