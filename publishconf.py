494181dd11874ebcf2f1c5d4d6d27f7bf6ba63f7
Sascha Markus <sascha.markus@gmail.com>
2019-01-07 21:23:31 +0100

    paths

--- publishconf.py
+++ publishconf.py
@@ -13,7 +13,7 @@ from pelicanconf import *
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 #SITEURL = 'https://www.der-flaneur.rocks'
 #SITEURL = ''
-SITEURL = 'https://saschamarkus.gitlab.io/der-flaneur-pelican'
+SITEURL = 'https://saschamarkus.gitlab.io/flaneur'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
@@ -27,6 +27,11 @@ M_LINKS_FOOTER1 = [('Der Flaneur', '/'),
 
 M_CSS_FILES = ['https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700,700i%7CSource+Code+Pro:400,400i,600',
                                path.join(SITEURL, '/static/m-light.css')]
+
+STATIC_PATHS = [
+                path.join(SITEURL, 'images'),
+                path.join(SITEURL, 'extra'),
+                ]
 
 DELETE_OUTPUT_DIRECTORY = True
 
