924bd1e178e42356ba15cf07ee6dfaed01b9c035
Sascha Markus <sascha.markus@gmail.com>
2019-06-04 04:49:42 +0000

    Publish at https://saschamarkus.gitlab.io/flaneur/

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://www.der-flaneur.rocks'
+#SITEURL = 'https://www.der-flaneur.rocks'
+SITEURL = 'https://saschamarkus.gitlab.io/flaneur/'
 # RELATIVE_URLS = True
 
 FEED_DOMAIN = SITEURL
