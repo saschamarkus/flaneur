d70ad739848b0ad9d4096d0a8ceead8d16214160
Sascha Markus <sascha.markus@gmail.com>
2019-06-04 05:27:50 +0000

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -13,7 +13,7 @@ from pelicanconf import *
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 #SITEURL = 'https://www.der-flaneur.rocks'
 SITEURL = 'https://saschamarkus.gitlab.io/flaneur/'
-# RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 FEED_DOMAIN = SITEURL
 
