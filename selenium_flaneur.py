fe4148ecd44256cc01e1e76cbaa6cd0693c3f24a
Sascha Markus <sascha.markus@gmail.com>
2019-06-13 15:28:01 +0000

    Update selenium_flaneur.py

--- selenium_flaneur.py
+++ selenium_flaneur.py
@@ -160,7 +160,8 @@ content_path = 'content/'
 driver = launch_browser()
 
 post_id, post_date, post_time = get_latest_post()
-bs_html = get_post(post_id)rst_date, file_date, fb_date = get_dates(post_date, post_time)
+bs_html = get_post(post_id)
+rst_date, file_date, fb_date = get_dates(post_date, post_time)
 
 img_path = download_image(driver, file_date)
 resize_image(img_path)
