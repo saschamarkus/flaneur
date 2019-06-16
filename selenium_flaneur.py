c016985a2daafa53be35ae6bae6b1a2819c963a3
Sascha Markus <sascha.markus@gmail.com>
2019-06-16 20:42:16 +0200

    allow emtpy lines after the last with content. It doesn't seem to be possibel to add linebreaks using the web editor in a mobile browser so I addded some in advance.

--- selenium_flaneur.py
+++ selenium_flaneur.py
@@ -38,7 +38,8 @@ def get_latest_post():
     p = ''
     with open('posts.txt') as posts:
         for line in posts:
-            p = line.replace('\n', '')
+            if line:
+                p = line.replace('\n', '')
     post_id, post_date, post_time = p.split(' ')
     return post_id, post_date, post_time
 
